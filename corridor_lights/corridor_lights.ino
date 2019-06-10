96bc8b8517867714c544bd7433af37e460bedc86
Grant Moe <grant.edward.moe@gmail.com>
2019-06-10 02:51:49 -0700

    Add alert states
    Add strobing red, orange, yellow, and none-alert lighting states

--- corridor_lights/corridor_lights.ino
+++ corridor_lights/corridor_lights.ino
@@ -9,18 +9,35 @@
 #define NUM_LEDS 30 // Number of LEDs in strip
 CRGB leds[ NUM_LEDS ];
 
+int wave_index = 0;
+
+struct alert_state {
+  CRGB color;
+  long period;
+  int min_value;
+  int max_value;
+};
+
+alert_state red_alert = {CRGB(255, 0, 0), 1000, 0, 127};
+alert_state orange_alert = {CRGB(255, 24, 0), 2000, 8, 127};
+alert_state yellow_alert = {CRGB(255, 128, 0), 3000, 16, 127};
+alert_state no_alert = {CRGB(255,255,255), 5000, 3, 4 };
+
+alert_state current_state;
+
 void setup() {
   FastLED.addLeds<LED_TYPE, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  current_state = orange_alert;
 }
 
 void loop() {
 
-  fill_solid(leds, NUM_LEDS, CHSV(0,255,64));
-  FastLED.show();
-  delay(1000);
+  if (++wave_index >= 256) {
+    wave_index = 0;
+  }
 
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-  FastLED.show();
-  delay(1000);                        
+  fill_solid(leds, NUM_LEDS, blend(CRGB::Black, current_state.color, map( cubicwave8(wave_index), 0, 255, current_state.min_value, current_state.max_value ) ) );
 
+  FastLED.show();
+  delay(current_state.period/255);
 }
