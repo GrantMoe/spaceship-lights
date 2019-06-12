bf09586b4eaa164c795f6305b04a57fca3e24d1a
Grant Moe <grant.edward.moe@gmail.com>
2019-06-12 07:32:33 -0700

    Add button to cycle through modes

--- corridor_lights/corridor_lights.ino
+++ corridor_lights/corridor_lights.ino
@@ -28,29 +28,43 @@ struct light_state {
 
 String input_mode;
 String current_mode;
+String modes[6] = {"no_alert", "yellow_alert", "orange_alert", "red_alert", "test_alert", "broken_state"};
+int mode_index = 0;
 
 light_state current_state;
 
 light_state red_alert = {CRGB(255, 0, 0), 0, 127, 1000, false};
-light_state orange_alert = {CRGB(255, 24, 0), 8, 127, 2000, true};
-light_state yellow_alert = {CRGB(255, 90, 0), 16, 127, 3000, true};
+light_state orange_alert = {CRGB(255, 48, 0), 8, 127, 2000, true};
+light_state yellow_alert = {CRGB(255, 128, 0), 16, 127, 3000, true};
 light_state no_alert = {CRGB(255,255,255), 3, 4, 5000, true};
 light_state broken_state = {CRGB(255,255,255), 0, 4, 5000, true};
 
-light_state test_alert = {CRGB(255, 20, 150), 8, 8, 5000, true};
+light_state test_alert = {CRGB(64, 128, 0), 0, 127, 1000, false};
 
 
 void setup() {
   FastLED.addLeds<LED_TYPE, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUM_LEDS);
 
-  input_mode = "red_alert";
-  current_state = no_alert;
+//  input_mode = "red_alert";
+  current_state = test_alert;
 
-  input_mode = "broken_state";
+  input_mode = "test_alert";
 //  Serial.begin(9600);
+
+  pinMode(5, INPUT_PULLUP);
+
 }
 
 void loop() {
+
+  if (digitalRead(5) == LOW) {
+          delay(500);
+
+    if (++mode_index >= 6) {
+      mode_index = 0;
+    }
+    input_mode = modes[mode_index];
+  }
 
   if (current_mode != input_mode) {
 
@@ -62,8 +76,8 @@ void loop() {
       current_state = orange_alert;
     } else if (input_mode == "yellow_alert") {
       current_state = yellow_alert;
-    } else if (input_mode == "custom_alert") {
-      // ADD CODE HERE
+    } else if (input_mode == "test_alert") {
+      current_state = test_alert;
     } else if (input_mode == "broken_state") {
       current_state = broken_state;
       for (int i = 0; i < NUM_LEDS; i++) {
@@ -78,7 +92,7 @@ void loop() {
 
   if (current_mode == "broken_state") {
     for (int i = 0; i < NUM_LEDS; i++) {
-      if (random(50000) <= 1) {
+      if (random(100000) <= 1) {
         flash_number[i] += random(5);
         flash_duration[i] = random(50, 1000);
       } 
