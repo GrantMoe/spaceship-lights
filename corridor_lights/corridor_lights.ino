7f562d153fa6c9e659340c14f611910d18177ccd
Grant Moe <grant.edward.moe@gmail.com>
2019-06-11 21:33:32 -0700

    Add random malfunction blinking behavior

--- corridor_lights/corridor_lights.ino
+++ corridor_lights/corridor_lights.ino
@@ -11,6 +11,7 @@
 CRGB leds[NUM_LEDS];
 int flash_number[NUM_LEDS];
 int flash_duration[NUM_LEDS];
+int last_flash[NUM_LEDS];
 
 unsigned long current_time = millis();
 unsigned long cycle_start = current_time;
@@ -25,28 +26,70 @@ struct light_state {
   bool throb;
 };
 
+String input_mode;
+String current_mode;
+
+light_state current_state;
+
 light_state red_alert = {CRGB(255, 0, 0), 0, 127, 1000, false};
 light_state orange_alert = {CRGB(255, 24, 0), 8, 127, 2000, true};
 light_state yellow_alert = {CRGB(255, 90, 0), 16, 127, 3000, true};
 light_state no_alert = {CRGB(255,255,255), 3, 4, 5000, true};
+light_state broken_state = {CRGB(255,255,255), 0, 4, 5000, true};
+
 light_state test_alert = {CRGB(255, 20, 150), 8, 8, 5000, true};
 
-light_state current_state;
 
 void setup() {
   FastLED.addLeds<LED_TYPE, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUM_LEDS);
-  current_state = red_alert;
 
+  input_mode = "red_alert";
+  current_state = no_alert;
+
+  input_mode = "broken_state";
 //  Serial.begin(9600);
 }
 
 void loop() {
 
+  if (current_mode != input_mode) {
+
+    current_mode = input_mode;
+
+    if (input_mode == "red_alert") {
+      current_state = red_alert;
+    } else if (input_mode == "orange_alert") {
+      current_state = orange_alert;
+    } else if (input_mode == "yellow_alert") {
+      current_state = yellow_alert;
+    } else if (input_mode == "custom_alert") {
+      // ADD CODE HERE
+    } else if (input_mode == "broken_state") {
+      current_state = broken_state;
+      for (int i = 0; i < NUM_LEDS; i++) {
+        flash_number[i] = 0;
+        flash_duration[i] = 0;
+      }
+    
+    } else {
+      current_state = no_alert;
+    }
+  }
+
+  if (current_mode == "broken_state") {
+    for (int i = 0; i < NUM_LEDS; i++) {
+      if (random(50000) <= 1) {
+        flash_number[i] += random(5);
+        flash_duration[i] = random(50, 1000);
+      } 
+    }
+  }
+
   current_time = millis();
 
   // set/reset cycle time
   if ((current_time - cycle_start) > current_state.period_duration) {
-    cycle_start = current_time;
+    cycle_start = current_time; 
   }
 
   // calculate brightness based percentage of cycle, scaled to 8 bits
@@ -54,6 +97,22 @@ void loop() {
 
 //  Serial.println(wave_index);
 
+  if (current_mode == "broken_state") {
+    for (int i = 0; i < NUM_LEDS; i++) {
+
+      if (flash_number[i] > 0) {
+        if ( (float(current_time) - float(last_flash[i])) > float(flash_duration[i]) ) {
+          last_flash[i] = current_time;
+          flash_number[i] = flash_number[i] - 1;
+        }
+        wave_index = ((float(current_time) - float(last_flash[i])) / float(flash_duration[i])) * 256;
+        leds[i] = blend(CRGB::Black, current_state.color, 
+                                    map(cubicwave8(wave_index), 0, 255, current_state.min_value, current_state.max_value ));
+      } else {
+        leds[i] = CRGB::Black;
+      }     
+    }
+  } else {
   // set LED colors
     if (current_state.throb) {
       fill_solid(leds, NUM_LEDS, blend(CRGB::Black, 
@@ -66,6 +125,7 @@ void loop() {
         fill_solid(leds, NUM_LEDS, CRGB::Black);
       }
     }
+  } 
 
   // show LEDs
   FastLED.show();
--- strandtest/strandtest.ino
+++ strandtest/strandtest.ino
@@ -42,16 +42,5 @@ int      head  = 0, tail = -10; // Index of first 'on' and 'off' pixels
 uint32_t color = 0xFF0000;      // 'On' color (starts red)
 
 void loop() {
-
-  strip.setPixelColor(head, color); // 'On' pixel at head
-  strip.setPixelColor(tail, 0);     // 'Off' pixel at tail
-  strip.show();                     // Refresh strip
-  delay(20);                        // Pause 20 milliseconds (~50 FPS)
-
-  if(++head >= NUMPIXELS) {         // Increment head index.  Off end of strip?
-    head = 0;                       //  Yes, reset head index to start
-    if((color >>= 8) == 0)          //  Next color (R->G->B) ... past blue now?
-      color = 0xFF0000;             //   Yes, reset to red
-  }
-  if(++tail >= NUMPIXELS) tail = 0; // Increment, reset tail index
+{ static uint8_t hue=0; leds.fill_rainbow(hue++); FastLED.delay(30); }
 }
